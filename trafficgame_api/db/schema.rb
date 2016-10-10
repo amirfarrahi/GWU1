6dc1654de27c3e5d3f6248d817d878499c1fea04
amir farrahi <a.m.farrahi@gmail.com>
2016-10-09 21:30:38 -0700

    changes1

--- trafficgame_api/db/schema.rb
+++ trafficgame_api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160821040405) do
+ActiveRecord::Schema.define(version: 20160905014258) do
 
   create_table "answers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "question_id"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 20160821040405) do
     t.string   "answer"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "bugs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "userid"
+    t.integer  "gameid"
+    t.string   "bugdetails"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "conditions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
